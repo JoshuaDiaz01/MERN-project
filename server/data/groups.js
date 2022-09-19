5aa3cd16d242ce83bacaed6266b4fc82ea8151bc
a-ruv <a.ruv009@gmail.com>
2022-09-19 17:30:45 -0400

    prices for data

--- server/data/groups.js
+++ server/data/groups.js
@@ -1,111 +1,138 @@
 groups = [
     {
         code: 01,
-        name: "Farm products"
+        name: "Farm products",
+        prices: [248.105, 255.503, 261.343, 263.817, 261.884, 250.299, 230.899, 220.224, 213.525, 206.125, 197.416, 197.868, 202.896, 201.463, 207.0, 211.7, 192.8, 184.3, 182.5, 178.8, 166.3, 173.0, 169.9, 157.0, 149.0, 151.4, 151.4, 148.8, 140.4, 157.5, 160.8, 169.5]
     },
     {
         code:02,
-        name: "Processed foods and feeds"
+        name: "Processed foods and feeds",
+        prices: [262.827, 261.972, 261.807, 259.988, 256.578, 250.367, 246.227, 241.889, 237.908, 237.801, 237.612, 239.343, 236.411, 232.370, 235.6, 231.1, 225.2, 219.8, 217.1, 213.4, 212.8, 213.8, 211.8, 210.0, 209.6, 210.2, 212.7, 224.1, 210.0, 209.3, 209.4, 210.7]
     },
     {
         code:03,
-        name: "Textile products and apparel"
+        name: "Textile products and apparel",
+        prices: [187.254, 186.065, 184.333, 182.834, 181.008, 177.402, 175.541, 174.218, 171.726, 171.265, 169.707, 166.970, 165.970, 165.203, 163.334, 160.9, 159.0, 157.6, 156.1, 153.4, 151.3, 149.9, 149.2, 149.3, 149.5, 149.5, 149.0, 149.1, 149.2, 149.1, 149.7, 150.0]
     },
     {
         code: 04,
-        name: "Hides, skins, leather, and realted products"
+        name: "Hides, skins, leather, and realted products",
+        prices: []
     },
     {
         code: 05,
-        name: "Fuels and related products and power"
+        name: "Fuels and related products and power",
+        prices: []
     },
     {
         code: 06,
-        name: "Chemicals and allies products"
+        name: "Chemicals and allies products",
+        prices: []
     },
     {
         code: 07,
-        name: "Rubber and plastic products"
+        name: "Rubber and plastic products",
+        prices: []
     },
     {
         code: 08,
-        name: "Lumber and wood products"
+        name: "Lumber and wood products",
+        prices: []
     },
     {
         code: 09,
-        name: "Pulp, paper, and allied products"
+        name: "Pulp, paper, and allied products",
+        prices: []
     },
     {
         code: 10,
-        name: "Metals and metal products"
+        name: "Metals and metal products",
+        prices: []
     },
     {
         code: 11,
-        name: "Machinery and equipment"
+        name: "Machinery and equipment",
+        prices: []
     },
     {
         code: 12,
-        name: "Furniture and househol durables"
+        name: "Furniture and househol durables",
+        prices: []
     },
     
     {
         code: 13,
-        name: "Nonmetallic mineral products"
+        name: "Nonmetallic mineral products",
+        prices: []
     },
     {
         code: 14,
-        name: "Transportation equipment"
+        name: "Transportation equipment",
+        prices: []
     },
     {
         code: 15,
-        name: "Miscellaneous products"
+        name: "Miscellaneous products",
+        prices: []
     },
     {
         code: 30,
-        name: "Transportation services"
+        name: "Transportation services",
+        prices: []
     },
     {
         code: 33,
-        name: "Publishing sales, excluding software"
+        name: "Publishing sales, excluding software",
+        prices: []
     },
     {
         code: 34,
-        name: "Software publishing"
+        name: "Software publishing",
+        prices: []
     },
     {
         code: 36,
-        name: "Advertising space and time sales"
+        name: "Advertising space and time sales",
+        prices: []
     },
     {
         code: 37,
-        name: "Telecommunication, cable, and internet user services"
+        name: "Telecommunication, cable, and internet user services",
+        prices: []
     },
     {
         code: 44,
-        name: "Rental and leasing of goods (partial)"
+        name: "Rental and leasing of goods (partial)",
+        prices: []
     },
     {
         code: 51,
-        name: "Health care services"
+        name: "Health care services",
+        prices: []
     },
     {
         code: 52,
-        name: "Educational services (partial)"
+        name: "Educational services (partial)",
+        prices: []
     },
     {
         code: 56,
-        name: "Entertainment services (partial)"
+        name: "Entertainment services (partial)",
+        prices: []
     },
     {
         code: 57,
-        name: "Wholesale trade services"
+        name: "Wholesale trade services",
+        prices: []
     },
     {
         code: 58,
-        name: "Retail trade services"
+        name: "Retail trade services",
+        prices: []
     },
     {
         code: 61,
-        name: "Contract work on textile products, apparel, and leather"
+        name: "Contract work on textile products, apparel, and leather",
+        prices: []
     },
 ]
