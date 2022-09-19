c40dce89faef1c0a789a302309a434eb31a2f2f2
aadowst <105029233+aadowst@users.noreply.github.com>
2022-09-19 13:39:01 -0700

    started theme builder

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,41 @@
 
 import './App.css';
 import { InteractiveList } from '../src/components/List'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import { CssBaseline, Typography } from '@mui/material'
 
 function App() {
+
+  const theme = createTheme({
+
+
+    palette: {
+      primary: {
+        main: '#292F4F'
+      },
+      warning: {
+        main: '#f24f5b'
+      },
+      success: {
+        main: '#26b58a'
+      },
+      secondary: {
+        main: '#b2aBF2'
+      },
+      info: {
+        main: '#f0e2a3'
+      }
+
+    }
+  })
   return (
     <div>
-      <h2>Inventori</h2>
-      <InteractiveList/>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Typography variant="h3" color="primary">Inventori</Typography>
+
+        <InteractiveList />
+      </ThemeProvider>
     </div>
   );
 }
