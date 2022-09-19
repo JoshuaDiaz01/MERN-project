ecca7095e636901303fb16f7491cf498b395af32
aadowst <105029233+aadowst@users.noreply.github.com>
2022-09-19 15:42:51 -0700

    added services

---
+++ client/src/services/categoryService.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const http = axios.create({
+    baseURL: 'http://localhost:8000/api'
+})
+
+const DB_NAME = "category_db"
+
+
+export const getAllCategories = async () => {
+    const res = await http.get(`/${DB_NAME}s`);
+    return res.data;
+}
+
+export const getCategoryByCode  = async (id) => {
+    const res = await http.get(`/${DB_NAME}s/${id}`);
+    return res.data;
+}
---
+++ client/src/services/inventoryService.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const http = axios.create({
+    baseURL: 'http://localhost:8000/api'
+})
+
+const DB_NAME = "inventory_db"
+
+export const createItem= async (data) => {
+    const res = await http.post(`/${DB_NAME}s`, data);
+    return res.data;
+}
+
+export const getAllItems = async () => {
+    const res = await http.get(`/${DB_NAME}s`);
+    return res.data;
+}
+
+export const getItemById  = async (id) => {
+    const res = await http.get(`/${DB_NAME}s/${id}`);
+    return res.data;
+}
+
+export const updateItemById  = async (id, data) => {
+    const res = await http.put(`/${DB_NAME}s/${id}`, data);
+    return res.data;
+}
+
+export const deleteItemById  = async (id) => {
+    const res = await http.delete(`/${DB_NAME}s/${id}`);
+    return res.data;
+}
