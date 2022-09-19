35524099ff13472e7dd5e50ba62a26fd27fb3035
a-ruv <a.ruv009@gmail.com>
2022-09-19 14:09:42 -0400

    group data and the beginning of API service Front end

--- server/models/item.model.js
+++ server/models/item.model.js
@@ -15,7 +15,7 @@ const ItemSchema = new mongoose.Schema(
         category: {
             type: String,
             required: [true, `{PATH} is required.`],
-         },
+        },
 
         isFavorited: {
             type: Boolean,
