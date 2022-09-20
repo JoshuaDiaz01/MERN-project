fe93eb7dd905be4c2fb0a9c77705320048199a71
JoshuaDiaz01 <joshuadiaz93117@gmail.com>
2022-09-20 10:34:35 -0700

    database edit

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,36 +1,14 @@
 const mongoose = require("mongoose");
 
-const dbOneName = "inventory_db";
-const dbTwoName = "category_db";
+mongoose
+  .connect(`mongodb://localhost/${dbName}`, {
 
-var conn      = mongoose.createConnection(`mongodb://localhost/${dbOneName}`);
-var conn2     = mongoose.createConnection(`mongodb://localhost/${dbTwoName}`);
-
-// stored in 'testA' database
-var Item = conn.model('Item', new mongoose.Schema({
-  title : { type : String, default : 'model in Item database' }
-}));
-
-// stored in 'testB' database
-var Category = conn2.model('Category', new mongoose.Schema({
-  title : { type : String, default : 'model in Category database' }
-}));
-
-
-
-
-
-
-
-// mongoose
-//   .connect(`mongodb://localhost/${dbName}`, {
-
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log(`Successfully connected to ${dbName}`);
-//   })
-//   .catch((error) =>
-//     console.log(`mongoose connection to ${dbName} failed:`, error)
-//   );
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`Successfully connected to ${dbName}`);
+  })
+  .catch((error) =>
+    console.log(`mongoose connection to ${dbName} failed:`, error)
+  );
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const port = 8000;
 
 const { itemRouter } = require("./routes/item.routes");
+const { categoryRouter } = require("./routes/category.routes");
 
 require("./config/mongoose.config");
 
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/items", itemRouter);
+app.use("/api/categories", categoryRouter);
 
 app.listen(port, () => 
     console.log(`Listening on port ${port} for requests to respond to`))
