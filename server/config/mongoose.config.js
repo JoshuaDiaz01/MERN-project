42a9b9560bf7190ad8527fc9cff90ea11aa07438
aadowst <105029233+aadowst@users.noreply.github.com>
2022-09-20 11:09:13 -0700

    fixed merge conflict

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -2,34 +2,15 @@ const mongoose = require("mongoose");
 
 const dbName = "inventori_db"
 
-var conn      = mongoose.createConnection(`mongodb://localhost/${dbOneName}`);
-var conn2     = mongoose.createConnection(`mongodb://localhost/${dbTwoName}`);
+mongoose
+  .connect(`mongodb://localhost/${dbName}`, {
 
-// stored in 'testA' database
-var Item = conn.model('Item', new mongoose.Schema({
-  title : { type : String, default : 'model in Item database' }
-}));
-
-// stored in 'testB' database
-var Category = conn2.model('Category', new mongoose.Schema({
-  title : { type : String, default : 'model in Category database' }
-}));
-
-
-
-
-
-
-
-// mongoose
-//   .connect(`mongodb://localhost/${dbName}`, {
-
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log(`Successfully connected to ${dbName}`);
-//   })
-//   .catch((error) =>
-//     console.log(`mongoose connection to ${dbName} failed:`, error)
-//   );
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`Successfully connected to ${dbName}`);
+  })
+  .catch((error) =>
+    console.log(`mongoose connection to ${dbName} failed:`, error)
+  );
