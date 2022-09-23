bef6a9847f4e35fa2b0bb5659ba159ac3cbd952b
aadowst <105029233+aadowst@users.noreply.github.com>
2022-09-23 11:48:13 -0700

    removed comments and fixed shadows

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 
 import './App.css';
 import { Main } from '../src/views/Main'
-import {ViewOne} from './views/ViewOne'
+import { ViewOne } from './views/ViewOne'
 import NavBar from "./components/NavBar";
 
-import { createTheme, ThemeProvider, CssBaseline, Typograpyh, Box} from '@mui/material';
+import { createTheme, ThemeProvider, CssBaseline, Box } from '@mui/material';
 import {
-  Link,
+
   Route,
-  Routes,
-  Navigate
+  Routes
+
 
 } from 'react-router-dom';
 import Footer from './components/Footer';
@@ -25,6 +25,10 @@ function App() {
       }
     },
     shadows: {
+      1: "0px 2px 1px -1px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14),0px 1px 3px 0px rgba(0,0,0,0.12)",
+      2: "0px 3px 1px -2px rgba(0,0,0,0.2),0px 2px 2px 0px rgba(0,0,0,0.14),0px 1px 5px 0px rgba(0,0,0,0.12)",
+      3: "0px 3px 3px -2px rgba(0,0,0,0.2),0px 3px 4px 0px rgba(0,0,0,0.14),0px 1px 8px 0px rgba(0,0,0,0.12)",
+      4: "0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)",
       8: "0px 5px 5px -3px rgba(0,0,0,0.4),0px 8px 10px 1px rgba(0,0,0,0.3),0px 3px 14px 2px rgba(0,0,0,0.2)"
     }
   })
@@ -38,19 +42,14 @@ function App() {
 
       <CssBaseline />
 
-      <Box sx={{backgroundColor: "primary.main"}}>
+      <Box sx={{ backgroundColor: "primary.main" }}>
         <NavBar></NavBar>
-        <Box sx={{padding: 3}}>
-        <Routes>
-          <Route path="/" element={<Main/>} />
-          <Route path="items/:id" element={ <ViewOne/> }/>
-        </Routes>
-        <Footer/>
-        {/* <Link to={`/items/632a38d1ba5831a78f9c5e0e`}>Apples</Link>
-        <Link to={`/items/632b519ac1556475c984ca2b`}>Gibson</Link> */}
-        {/* <ViewOne itemData={itemData}/> */}
-
-        
+        <Box sx={{ padding: 3 }}>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="items/:id" element={<ViewOne />} />
+          </Routes>
+          <Footer />
         </Box>
       </Box>
     </ThemeProvider>
