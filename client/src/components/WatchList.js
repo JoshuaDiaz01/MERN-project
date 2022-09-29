f3d156338ab5495c184dd15827b414f853b08a89
aadowst <105029233+aadowst@users.noreply.github.com>
2022-09-29 10:27:31 -0700

    incorporated service and fixed formatting

--- client/src/components/WatchList.js
+++ client/src/components/WatchList.js
@@ -3,17 +3,19 @@ import Paper from '@mui/material/Paper';
 import { Box, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import {InflationDisplay} from '../components/InflationDisplay';
+import { InflationDisplay } from '../components/InflationDisplay';
+import { updateItemById } from '../services/inventoryService';
 
 export const WatchList = (props) => {
     const { inventory, updateInventoryItem, categories } = props
     const navigate = useNavigate();
+
     const toggleFavorite = async (item) => {
         const updatedItem = { ...item, isFavorited: !item.isFavorited }
-
-        axios.put('http://localhost:8000/api/items/' + item._id, updatedItem)
-            .then((res) => updateInventoryItem(res.data))
+        await updateItemById(item._id, updatedItem)
+            .then((res) => updateInventoryItem(res))
             .catch((error) => console.log(error))
+
     }
 
     const handleItemClick = (id) => {
@@ -24,7 +26,6 @@ export const WatchList = (props) => {
         <>
             <Paper elevation={8} sx={{ padding: 1, paddingRight: 3 }}>
                 <Typography variant="h5" align="right">Watch List</Typography>
-                {/* <Typography variant="h6" align="center" sx={{ marginRight: 5 }}>In Inventory</Typography> */}
 
                 <List>
                     {
@@ -32,26 +33,25 @@ export const WatchList = (props) => {
 
                             return (
                                 <ListItem key={index} >
+
                                     <ListItemIcon sx={{ width: "10%" }} onClick={(e) => toggleFavorite(entry)}>
                                         <IconButton><StarIcon fontSize={'medium'} color="primary" /></IconButton>
                                     </ListItemIcon>
-                                    <ListItemButton>
 
+                                    <ListItemButton>
                                         <ListItemText key={index} onClick={() => handleItemClick(entry._id)}>{entry.name}</ListItemText>
                                     </ListItemButton>
 
                                     <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
-
-                                    <InflationDisplay sx={{ width: "10%" }} categories={categories} groupCode={entry.category} secondary={""}/>
-                                        
+                                        <InflationDisplay categories={categories} groupCode={entry.category} secondary={""} />
                                     </Box>
 
                                 </ListItem>
                             )
+
                         })
                     }
                 </List>
-                {/* <Typography variant="h6" align="center" sx={{ marginRight: 5 }}>Not In Inventory</Typography> */}
             </Paper>
         </>
     )
