ae4ad5560310bb9d2fd8ea7448a4a32b5101bc5a
aadowst <105029233+aadowst@users.noreply.github.com>
2022-09-30 10:59:41 -0700

    removed console logs

--- server/services/item.service.js
+++ server/services/item.service.js
@@ -1,7 +1,6 @@
 const { Item } = require("../models/item.model");
 
 const createItem = async (data) => {
-    console.log("service: createItem");
     const item = await Item.create(data);
     console.log(item);
     return item;
@@ -9,37 +8,29 @@ const createItem = async (data) => {
 
 const getAllItems = async () => {
     const allItems = await Item.find();
-
     return allItems
 }
 
 const getItemById = async (id) => {
     const oneItem = await Item.findById(id);
-
     return oneItem;
 }
 
 const deleteItemById = async (id) => {
     const deletedItem = await Item.findByIdAndDelete(id);
-
     return deletedItem;
 }
 
 const updateItemById = async (id, data) => {
-    console.log("service: updateItemById");
-    console.log("updateAuthoerById data: " + JSON.stringify(data));
     const updatedItem = await Item.findByIdAndUpdate(id, data, {
         //displays updated data after original 
         new: true
     });
-
     return updatedItem
 }
 
 const updateOrderHistory = async (id, data) => {
-    console.log("do i ever get here", data.orderHistory[0]);
     const updatedItem = await Item.updateOne({_id: id}, {$push: { orderHistory: data.orderHistory[0] }})
-
     return updatedItem;
 }
 
